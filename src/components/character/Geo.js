d3e72583e43f2922be460178dc9040e1a4dca4f1
Bohdan12367 <lukasevicboda@gmail.com>
2021-08-11 13:46:46 +0300

    shablon

--- src/components/character/Geo.js
+++
@@ -1,8 +0,0 @@
-export default function Geo({geo}){
-    return(
-        <div className={'geo'}>
-            <b>{geo.address.geo.lat}</b>
-            <b>{geo.address.geo.lng}</b>
-        </div>
-    )
-}
